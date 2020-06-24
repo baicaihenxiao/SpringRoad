9e7a13a5672eaf063255bd5a5fcbedc4f784c1f2
baicaihenxiao <baicaihenxiao@gmail.com>
2020-06-24 21:53:56 +0800

    feat: bean生命周期

---
+++ src/main/java/club/cser/springroad/beans/BeanLifeCycle.java
@@ -0,0 +1,34 @@
+package club.cser.springroad.beans;
+
+public class BeanLifeCycle {
+
+    private String cycle;
+
+    @Override
+    public String toString() {
+        return "BeanLifeCycle{" +
+                "cycle='" + cycle + '\'' +
+                '}';
+    }
+
+    public String getCycle() {
+        return cycle;
+    }
+
+    public void setCycle(String cycle) {
+        System.out.println("settingg method..." + cycle);
+        this.cycle = cycle;
+    }
+
+    public BeanLifeCycle() {
+        System.out.println("constructt method..." + cycle);
+    }
+
+    public void initttt() {
+        System.out.println("initt method..." + cycle);
+    }
+
+    public void destroyyyy() {
+        System.out.println("destroyy method..." + cycle);
+    }
+}
---
+++ src/main/java/club/cser/springroad/beans/client/BeanCycleClient.java
@@ -0,0 +1,18 @@
+package club.cser.springroad.beans.client;
+
+import club.cser.springroad.beans.BeanLifeCycle;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanCycleClient {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+
+        BeanLifeCycle cycle1 = (BeanLifeCycle) applicationContext.getBean("cycle1");
+        System.out.println(cycle1);
+
+        ((ConfigurableApplicationContext)applicationContext).close();
+    }
+}
