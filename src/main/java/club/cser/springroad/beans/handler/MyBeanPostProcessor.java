f4190efe2106289675ec9556e2812b74cf33fa06
baicaihenxiao <baicaihenxiao@gmail.com>
2020-06-26 20:13:33 +0800

    feat: BeanPostProcessor

---
+++ src/main/java/club/cser/springroad/beans/handler/MyBeanPostProcessor.java
@@ -0,0 +1,18 @@
+package club.cser.springroad.beans.handler;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+public class MyBeanPostProcessor implements BeanPostProcessor {
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("postProcessBeforeInitialization: bean=" + bean + " beanName=" + beanName);
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("postProcessAfterInitialization: bean=" + bean + " beanName=" + beanName);
+        return bean;
+    }
+}
